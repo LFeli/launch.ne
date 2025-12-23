57f09d5901710e962e95672f846fcb24426df2e0
Lucas Araújo <lucasfelipemk@gmail.com>
2025-12-23 17:15:48 -0300

    ✨ feat(constant): added a constant for setup some constants for use in `site` in general

---
+++ src/constants/site.ts
@@ -0,0 +1,20 @@
+import { envClient } from '@/lib/env/client'
+import { extractDomain } from '@/lib/extract-domain'
+
+/**
+ * Frontend domain extracted from the public application URL.
+ */
+const FRONTEND_URL = extractDomain({
+  url: envClient.NEXT_PUBLIC_APP_URL,
+})
+
+/**
+ * Backend URL extracted from the public application URL,
+ * including the pathname.
+ */
+const BACKEND_URL = extractDomain({
+  url: envClient.NEXT_PUBLIC_APP_URL,
+  includePathname: true,
+})
+
+export { FRONTEND_URL, BACKEND_URL }
